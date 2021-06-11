9963d03daef9f05ed16762007730501d7ff06519
Your NameTheOxFromOutOfTheBox <himanshukotamarthi@gmail.com>
2021-06-11 15:58:03 +0530

    made home page

--- src/index.js
+++ src/index.js
@@ -1,22 +1,58 @@
-import React from "react";
+import React,{useState} from "react";
 import ReactDOM from "react-dom";
-import "./index.css"
-import SectorButton from "./sectorButton.js"
+import "./index.css";
+import SectorButton from "./sectorButton.js";
+import {Container,Row,Col} from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./navbar.js"
 
 function FullWidthGrid() {
-  // const classes = useStyles();
+  // const [color,setColor]=useState("")
+  let color=""
 
   return (
-    <div>
-      Hi0
-      <div style={{width:"80%"}}>
-        By1
-      </div>
-      <div style={{width:"20%"}}>
-        Get1
-      </div>
-      <SectorButton></SectorButton>
-    </div>
+    <>
+      <NavBar></NavBar>
+      <Row noGutters>
+        <Col sm={12} md={8} className="text-center mt-2">
+          sm=12 md=8
+          {/* <div style={{ textAlign:"center" }}> */}
+          <SectorButton name="Primary Sector" color="primary"></SectorButton>
+          <SectorButton name="Secondary Sector" color="info"></SectorButton>
+          <SectorButton name="Tertiary Sector" color="warning"></SectorButton>
+          <SectorButton name="Game Mode 1" color="danger"></SectorButton>
+          {/* </div> */}
+        </Col>
+        <Col sm={12} md={4} className="text-center mt-2">
+          <div
+            style={{
+              margin: "10%",
+              border: "2px solid black",
+              backgroundColor: "#21ED7F",
+            }}
+          >
+            <h2>Stats</h2>
+            {/* <ul style={{ listStyleType: "none" }}>
+              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
+              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
+              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
+              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
+              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
+              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
+            </ul> */}
+            <div style={{ marginTop: "30px" }}>
+              <h4 style={{ marginBottom: "20px" }}>Param 1=Value 1</h4>
+              <h4 style={{ marginBottom: "20px" }}>Param 1=Value 1</h4>
+              <h4 style={{ marginBottom: "20px" }}>Param 1=Value 1</h4>
+              <h4 style={{ marginBottom: "20px" }}>Param 1=Value 1</h4>
+              <h4 style={{ marginBottom: "20px" }}>Param 1=Value 1</h4>
+              <h4 style={{ marginBottom: "20px" }}>Param 1=Value 1</h4>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      {/* </Container> */}
+    </>
   );
 }
 
--- src/sectorButton.js
+++ src/sectorButton.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button'
+import {Button,ProgressBar} from 'react-bootstrap'
 const SectorButton = (props) => {
-    return ( 
-      <>
-        <Button variant="primary" size="lg" block>
-          Block level button
+    let color=props.color
+    let name=props.name
+    let color1="success"
+    return (
+      <div style={{ width: "50%", margin: "5% auto" }}>
+        <Button style={{ marginBottom: "1%",color:"black",fontWeight:"bold" }} variant={color} size="lg" block>
+          {name}
         </Button>
-      </>
+          <ProgressBar
+            variant={color1}
+            now={60}
+            label={"now"}
+            striped
+          >
+        </ProgressBar>
+      </div>
     );
 }
  
