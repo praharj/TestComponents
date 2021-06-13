485e3a7a0f0415f2996808acb7e7f8c37f8ca484
Your NameTheOxFromOutOfTheBox <himanshukotamarthi@gmail.com>
2021-06-13 11:07:19 +0530

    Started quizApp

--- src/index.js
+++ src/index.js
@@ -16,12 +16,10 @@ function FullWidthGrid() {
       <Row noGutters>
         <Col sm={12} md={8} className="text-center mt-2">
           sm=12 md=8
-          {/* <div style={{ textAlign:"center" }}> */}
           <SectorButton name="Primary Sector" color="primary"></SectorButton>
           <SectorButton name="Secondary Sector" color="info"></SectorButton>
           <SectorButton name="Tertiary Sector" color="warning"></SectorButton>
           <SectorButton name="Game Mode 1" color="danger"></SectorButton>
-          {/* </div> */}
         </Col>
         <Col sm={12} md={4} className="text-center mt-2">
           <div
@@ -32,14 +30,6 @@ function FullWidthGrid() {
             }}
           >
             <h2>Stats</h2>
-            {/* <ul style={{ listStyleType: "none" }}>
-              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
-              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
-              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
-              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
-              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
-              <li style={{ marginTop: "30px" }}>Param 1=Value 1</li>
-            </ul> */}
             <div style={{ marginTop: "30px" }}>
               <h4 style={{ marginBottom: "20px" }}>Param 1=Value 1</h4>
               <h4 style={{ marginBottom: "20px" }}>Param 1=Value 1</h4>
@@ -51,9 +41,16 @@ function FullWidthGrid() {
           </div>
         </Col>
       </Row>
-      {/* </Container> */}
     </>
   );
 }
 
-ReactDOM.render(<FullWidthGrid />, document.getElementById("root"));
+function QuizApp(){
+  return(
+    <div>
+      Hi
+    </div>
+  )
+}
+
+ReactDOM.render(<QuizApp />, document.getElementById("root"));
