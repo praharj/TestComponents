c45d4e90e5ada98613d45928fc1c98cceb4c09bb
Ulorewien <rohanshingre7@gmail.com>
2021-06-13 13:13:35 +0530

    added explanation

---
+++ src/explanation.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Explanation() {
+  return (
+    <div style={{ marginTop: "5%" }}>
+      <h1>Explanation</h1>
+      <p>
+        Burton "Gus" Guster is a fictional character on the USA Network
+        television comedy Psych played by American actor Dulé Hill. He functions
+        as the "straight man" for Shawn Spencer's antics, and provides sobering
+        advice, helpful knowledge, steady support, and friendship. Over the
+        course of the show, Gus evolves from nothing more than a conventional,
+        uptight pharmaceutical representative to a strong support system through
+        all Shawn Spencer's crazy antics. He frequently tries to pass himself
+        off as "fearless" although in reality the opposite is true, and often
+        cries, sometimes due to "sympathy" and other times when Shawn is
+        insensitive to his feelings. It is believed that Gus was named after a
+        relative of Steve Franks, the technical producer of the show, Burton
+        Franks.
+      </p>
+    </div>
+  );
+}
+
+export default Explanation;
--- src/quizapp.js
+++ src/quizapp.js
@@ -2,16 +2,18 @@ import React from "react";
 import Score from "./score";
 import Field from "./field";
 import Sectors from "./sectors";
+import Explanation from "./explanation";
 
 function QuizApp() {
   return (
     <div
-      style={{ backgroundColor: "aqua", height: "100vh" }}
+      style={{ backgroundColor: "aqua", height: "fit-content" }}
       className="container"
     >
       <Score />
       <Field />
       <Sectors />
+      <Explanation />
     </div>
   );
 }
