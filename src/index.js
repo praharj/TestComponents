244c3e4b73cc1e487f328a53eed94314b7be7bf7
Your NameTheOxFromOutOfTheBox <himanshukotamarthi@gmail.com>
2021-06-16 14:29:27 +0530

    Started idleclicker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
+import { React, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { Row,Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import NavBar from "./navbar.js";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./home";
 import QuizApp from "./quizapp";
+import { Button } from "react-bootstrap";
 
 // function App() {
 //   return (
@@ -19,14 +20,14 @@ import QuizApp from "./quizapp";
 //     </BrowserRouter>
 //   );
 // }
-function QuizApp1(){
-  function checkAns(){
-      let correctAns=document.getElementById("opt3")
-      correctAns.style.backgroundColor="chartreuse"
-      let explanation=document.getElementById("explanation")
-      explanation.style.display="block"
-      let correctDot=document.getElementById("progress0")
-      correctDot.style.backgroundColor="chartreuse"
+function QuizApp1() {
+  function checkAns() {
+    let correctAns = document.getElementById("opt3");
+    correctAns.style.backgroundColor = "chartreuse";
+    let explanation = document.getElementById("explanation");
+    explanation.style.display = "block";
+    let correctDot = document.getElementById("progress0");
+    correctDot.style.backgroundColor = "chartreuse";
   }
   return (
     <div>
@@ -140,7 +141,7 @@ function QuizApp1(){
         <div
           id="nextbtn"
           className="container"
-          style={{ backgroundColor: "aquamarine"}}
+          style={{ backgroundColor: "aquamarine" }}
         >
           <button>Next</button>
         </div>
@@ -148,4 +149,218 @@ function QuizApp1(){
     </div>
   );
 }
-ReactDOM.render(<QuizApp1 />, document.getElementById("root"));
+function Trial() {
+  const [count, setCount] = useState(0);
+  function decrement() {
+    if (count === 0) {
+      setCount((prevCount) => prevCount);
+    } else {
+      setCount((prevCount) => prevCount - 1);
+    }
+  }
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Hi</h1>
+      <Button onClick={decrement}>-</Button>
+      <span
+        style={{ width: "50px", display: "inline-block", textAlign: "center" }}
+      >
+        {count}
+      </span>
+      <Button onClick={increment}>+</Button>
+    </div>
+  );
+}
+
+function IdleClicker() {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: "chartreuse",
+          textAlign: "center",
+          padding: "1.5% 0",
+        }}
+      >
+        <Button>Stats</Button>
+        <div
+          style={{
+            textAlign: "center",
+            display: "inline-block ",
+            width: "50%",
+          }}
+        >
+          952,100,111
+        </div>
+        <div style={{ textAlign: "right", display: "inline-block" }}>
+          <Button>Managers</Button>
+        </div>
+      </div>
+      <div
+        style={{
+          justifyContent: "center",
+          backgroundColor: "lightblue",
+          padding: "1.25%",
+        }}
+      >
+        <ul
+          style={{
+            listStyleType: "none",
+            textAlign: "center",
+            padding: "0",
+            margin: "0",
+          }}
+        >
+          <li
+            style={{
+              display: "inline",
+              padding: "1.25%",
+              backgroundColor: "grey",
+              borderLeft: "2px solid black",
+              borderBottom: "2px solid black",
+              color: "white",
+            }}
+          >
+            Primary
+          </li>
+          <li
+            style={{
+              display: "inline",
+              padding: "1.25%",
+              backgroundColor: "grey",
+              borderLeft: "2px solid black",
+              borderBottom: "2px solid black",
+              color: "white",
+            }}
+          >
+            Secondary
+          </li>
+          <li
+            style={{
+              display: "inline",
+              padding: "1.25%",
+              backgroundColor: "grey",
+              borderLeft: "2px solid black",
+              borderBottom: "2px solid black",
+              color: "white",
+            }}
+          >
+            Tertiary
+          </li>
+        </ul>
+      </div>
+      <div>
+        <img
+          style={{
+            border: "1px solid black",
+            borderRadius: "50%",
+            objectFit: "contain",
+            width: "150px",
+            height: "150px",
+          }}
+          src="https://i.ibb.co/bKHs80C/rice.jpg"
+        ></img>
+      </div>
+      <div>
+        <img
+          style={{
+            border: "1px solid black",
+            borderRadius: "50%",
+            objectFit: "contain",
+            width: "150px",
+            height: "150px",
+          }}
+          src="https://i.ibb.co/bKHs80C/rice.jpg"
+        ></img>
+      </div>
+      <div>
+        <img
+          style={{
+            border: "1px solid black",
+            borderRadius: "50%",
+            objectFit: "contain",
+            width: "150px",
+            height: "150px",
+          }}
+          src="https://i.ibb.co/bKHs80C/rice.jpg"
+        ></img>
+      </div>
+      <div>
+        <img
+          style={{
+            border: "1px solid black",
+            borderRadius: "50%",
+            objectFit: "contain",
+            width: "150px",
+            height: "150px",
+          }}
+          src="https://i.ibb.co/bKHs80C/rice.jpg"
+        ></img>
+      </div>
+      <div>
+        <img
+          style={{
+            border: "1px solid black",
+            borderRadius: "50%",
+            objectFit: "contain",
+            width: "150px",
+            height: "150px",
+          }}
+          src="https://i.ibb.co/bKHs80C/rice.jpg"
+        ></img>
+      </div>
+      <div>
+        <img
+          style={{
+            border: "1px solid black",
+            borderRadius: "50%",
+            objectFit: "contain",
+            width: "150px",
+            height: "150px",
+          }}
+          src="https://i.ibb.co/bKHs80C/rice.jpg"
+        ></img>
+      </div>
+      <div>
+        <img
+          style={{
+            border: "1px solid black",
+            borderRadius: "50%",
+            objectFit: "contain",
+            width: "150px",
+            height: "150px",
+          }}
+          src="https://i.ibb.co/bKHs80C/rice.jpg"
+        ></img>
+      </div>
+      <div>
+        <img
+          style={{
+            border: "1px solid black",
+            borderRadius: "50%",
+            objectFit: "contain",
+            width: "150px",
+            height: "150px",
+          }}
+          src="https://i.ibb.co/bKHs80C/rice.jpg"
+        ></img>
+      </div>
+      <div>
+        <img
+          style={{
+            border: "1px solid black",
+            borderRadius: "50%",
+            objectFit: "contain",
+            width: "150px",
+            height: "150px",
+          }}
+          src="https://i.ibb.co/bKHs80C/rice.jpg"
+        ></img>
+      </div>
+    </div>
+  );
+}
+ReactDOM.render(<IdleClicker />, document.getElementById("root"));
--- src/quizapp.js
+++ src/quizapp.js
@@ -1,19 +1,128 @@
-import React from "react";
-import Score from "./score";
-import Field from "./field";
-import Sectors from "./sectors";
-import Explanation from "./explanation";
+import {React,useState} from "react";
+import {Badge,Button} from "react-bootstrap"
+
 
 function QuizApp() {
+  const [score, setScore] = useState(0);
+  const [selectedAns, setSelectedAns] = useState("opt3");
+  let correctAns="opt3"
+  const handleClick =()=>{
+    if (correctAns === selectedAns) {
+      setScore((prevScore) => prevScore + 10);
+      console.log("Done");
+    } else {
+      setScore((prevScore) => prevScore - 5);
+      console.log("Undone");
+    }
+  }
+  var timeleft = 20;
+  var timeleftstr = "";
+  function timer(){
+    document.getElementById("startbtn").setAttribute("disabled","")
+  var timerid=setInterval(function(){
+    if (timeleft<=0){
+      clearInterval(timerid)
+    }else{
+      timeleft-=1
+      var minutes=Math.floor(timeleft/60);
+      var seconds=timeleft%60;
+      var secondsstr="" 
+      if(seconds<10){
+        secondsstr="0"+JSON.stringify(seconds)
+      }
+      else{
+        secondsstr=JSON.stringify(seconds)
+      }
+      timeleftstr=JSON.stringify(minutes)+":"+secondsstr
+    }
+    console.log(timeleftstr)
+    document.getElementById("timer").innerHTML="Time Left-"+timeleftstr
+  },1000)
+  }
+
+
+
   return (
     <div
       style={{ backgroundColor: "aqua", height: "fit-content" }}
       className="container"
     >
-      <Score />
-      <Field />
-      <Sectors />
-      <Explanation />
+      <div style={{ textAlign: "right" }}>
+        <h1>
+          <Badge style={{ width: "250px" }} id="timer" variant="secondary">
+            Timer
+          </Badge>
+        </h1>
+        <h1>
+          <Badge style={{ width: "250px" }} variant="secondary">
+            Score-{score}
+          </Badge>
+        </h1>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <img
+          style={{
+            border: "1px solid black",
+            borderRadius: "4px",
+            padding: "5px",
+            width: "320px",
+            marginTop: "30px",
+          }}
+          src="https://i.pinimg.com/600x315/89/c3/ec/89c3ec21ef6ca277d9108db0c3339f77.jpg"
+          alt="gus"
+        ></img>
+        <h1 id="img-title">Gus Dancing</h1>
+      </div>
+      <Button id="startbtn" onClick={timer}>
+        Start
+      </Button>
+      {/* <Button id="endbtn" onClick={ () =>{clearInterval(timerid)}}>
+        End
+      </Button> */}
+      <div style={{ textAlign: "center" }}>
+        <Button
+          onClick={handleClick}
+          className="choicebtn"
+          id="primarybtn"
+          size="lg"
+          variant="light"
+        >
+          Primary
+        </Button>
+        <Button
+          onClick={handleClick}
+          className="choicebtn"
+          size="lg"
+          variant="light"
+        >
+          Secondary
+        </Button>
+        <Button
+          onClick={handleClick}
+          className="choicebtn"
+          size="lg"
+          variant="light"
+        >
+          Tertiary
+        </Button>
+      </div>
+      <div style={{ marginTop: "5%" }}>
+        <h1>Explanation</h1>
+        <p>
+          Burton "Gus" Guster is a fictional character on the USA Network
+          television comedy Psych played by American actor Dulé Hill. He
+          functions as the "straight man" for Shawn Spencer's antics, and
+          provides sobering advice, helpful knowledge, steady support, and
+          friendship. Over the course of the show, Gus evolves from nothing more
+          than a conventional, uptight pharmaceutical representative to a strong
+          support system through all Shawn Spencer's crazy antics. He frequently
+          tries to pass himself off as "fearless" although in reality the
+          opposite is true, and often cries, sometimes due to "sympathy" and
+          other times when Shawn is insensitive to his feelings. It is believed
+          that Gus was named after a relative of Steve Franks, the technical
+          producer of the show, Burton Franks.
+        </p>
+      </div>
     </div>
   );
 }
