01d0a42cdfbbdf2abd262bef7087d79f8e48007c
Shubh Pathak <shubhpathak07@gmail.com>
2022-10-04 23:28:37 +0530

    feat: added button component

---
+++ src/button.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+         <button>
+            New-Button Heavy
+        </button>
+    </div>
+  );
+};
+
+export default Button;
+
